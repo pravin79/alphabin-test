a51dd8a5ecb5b18c985276d370cff0a87903c2bb
Pravin Gamit <praving@valuefy.com>
2023-05-31 15:11:48 +0530

    Updated settings.py and apiViews.py files

--- demoApp/apiViews.py
+++ demoApp/apiViews.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from django.contrib.auth.models import User
 from .models import Cart
