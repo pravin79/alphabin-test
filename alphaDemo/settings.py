7ba2faedb444687f8faf6262fd10f4d0df585b95
Pravin Gamit <praving@valuefy.com>
2023-07-07 16:34:59 +0530

    Mysql database on the floor1

--- alphaDemo/settings.py
+++ alphaDemo/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'alphaDemo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alphabin',
+        'NAME': 'mysql',
         'USER': 'root',
         'PASSWORD': '1234',
         'HOST': 'localhost',
